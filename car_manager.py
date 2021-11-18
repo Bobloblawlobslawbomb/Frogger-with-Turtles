2e7b5bd5cb7d27def96b232f95f2b124219d4d92
Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>
2021-11-17 16:09:44 -0800

    add random chance to car gneration

--- car_manager.py
+++ car_manager.py
@@ -12,12 +12,14 @@ class CarManager(Turtle):
         self.car_list = []
 
     def add_car(self):
-        new_car = Turtle("square")
-        new_car.color(choice(COLORS))
-        new_car.shapesize(stretch_wid=1, stretch_len=randint(2, 3))
-        new_car.penup()
-        new_car.goto(295, randint(-250, 250))
-        self.car_list.append(new_car)
+        chance = randint(1, 6)
+        if chance == 1:
+            new_car = Turtle("square")
+            new_car.color(choice(COLORS))
+            new_car.shapesize(stretch_wid=1, stretch_len=randint(2, 3))
+            new_car.penup()
+            new_car.goto(295, randint(-250, 250))
+            self.car_list.append(new_car)
 
     def move_cars(self):
         for car in self.car_list:
