b82aad3d9c9a55e84fc4c433f9abc4bcfb558b05
Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>
2021-11-17 16:36:02 -0800

    add speed_up method

--- car_manager.py
+++ car_manager.py
@@ -3,13 +3,16 @@ from random import randint, choice
 
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
-MOVE_INCREMENT = 10
+
+CAR_SPEED = 0.1
+CAR_SPEED_MULTIPLIER = 0.9
 
 
 class CarManager(Turtle):
     def __init__(self):
         super().__init__()
         self.car_list = []
+        self.speed = CAR_SPEED
 
     def add_car(self):
         chance = randint(1, 6)
@@ -24,3 +27,6 @@ class CarManager(Turtle):
     def move_cars(self):
         for car in self.car_list:
             car.backward(STARTING_MOVE_DISTANCE)
+
+    def speed_up(self):
+        self.speed *= CAR_SPEED_MULTIPLIER
